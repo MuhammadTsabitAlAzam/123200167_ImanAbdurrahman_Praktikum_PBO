55aefdbe1bbabebe03e68af848f043d674d99354
Iman Abdurrahman <namasayaiman02@gmail.com>
2022-04-23 14:49:37 +0700

    Tugas 4

---
+++ Tugas_4/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    private static JFrame loginView;
+
+
+    public static void main(String[] args) {
+	    loginView = new LoginView("Login");
+    }
+}
